864f41209c472d0fa0d95d54ae97d379a90e6b7f
BetaKrankri <edominguezj11@gmail.com>
2023-01-05 19:33:08 -0600

    mock up data added

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,8 +4,17 @@ import Title from '../Title/Title';
 import AddBar from '../AddBar/AddBar';
 import ToDoList from '../ToDoList/ToDoList';
 
+/*Mock up Data*/
+import mockup from '../../util/mockup-data.js'
+
 function App() {
+
+  
+
+
+
   return (
+
     <div className='App'>
         <Title text='My To Do List'/>
         <ToDoList onDelete={() => {}} onToggleCheck ={() => {}}/>
---
+++ src/util/mockup-data.js
@@ -0,0 +1,21 @@
+const mockup = [
+    {
+        id: 0,
+        task: 'Go to a walk',
+        done: false,
+    }, {
+        id: 1,
+        task: 'Make ma bed',
+        done: true
+    }, {
+        id: 2,
+        task: 'Breakfast',
+        done: true
+    }, {
+        id: 4,
+        task: 'Take out garbage',
+        done: false
+    }
+]
+
+export default mockup;
